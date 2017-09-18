2ca0b07340c855169235c032f34b855a1a6be31e
N <limepassion@gmail.com>
2017-09-18 14:19:35 +0300

    Ad - some fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
+  has_many :ads
+
   extend Enumerize
   enumerize :role, in: [:guest, :user, :admin], default: :guest 
 
-    devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :ads
+  # :confirmable, :lockable, :timeoutable and :omniauthable 
 
 end
