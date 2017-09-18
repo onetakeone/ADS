2cd291227d637b870c932edaf16eadb45cbe36b7
N <limepassion@gmail.com>
2017-09-18 14:07:01 +0300

    Ad model: belongs_to, has_many

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   extend Enumerize
   enumerize :role, in: [:guest, :user, :admin], default: :guest 
-  
+
+    devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  has_many :ads
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170918092900) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_ads_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
