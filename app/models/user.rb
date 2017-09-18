da129d30001f6dbd8f4a3cfc5f0c4f518381f03d
N <limepassion@gmail.com>
2017-09-18 13:39:50 +0300

    view edit ( header username, edit user username)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   extend Enumerize
   enumerize :role, in: [:guest, :user, :admin], default: :guest 
-
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
