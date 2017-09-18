19ba36070aeec92ad61d37f37571f68c10e4eb4c
N <limepassion@gmail.com>
2017-09-18 13:23:05 +0300

    user edit (post route, form, redirect)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+
   def index
     @users = User.all
   end
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
+    redirect_to users_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root "ads#index"
   get "users", to: "users#index"
   get "users/:id/edit", to: "users#edit"
-  
+  post "users/:id/edit", to: "users#update"
+
   resources :ads
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
