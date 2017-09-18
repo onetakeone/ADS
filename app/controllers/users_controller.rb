d991bb8378c66f93e2b4a453a98917bf204b9d6d
N <limepassion@gmail.com>
2017-09-18 13:07:52 +0300

    cntrl User update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,21 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username,:role,:email,:password)
+  end
+
 
 end
