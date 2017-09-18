cc25feaf14dca6ebf3afc9a5ec0b9668f9960130
N <limepassion@gmail.com>
2017-09-18 12:47:54 +0300

    edit Ad migration

--- db/migrate/20170918092900_create_ads.rb
+++ db/migrate/20170918092900_create_ads.rb
@@ -1,7 +1,10 @@
 class CreateAds < ActiveRecord::Migration[5.1]
   def change
     create_table :ads do |t|
-
+      t.integer :user_id
+      t.string :title
+      t.text :body
+      t.string :image      
       t.timestamps
     end
   end
