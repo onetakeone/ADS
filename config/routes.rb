f988d2b58fc29d02ce1c3f0fd7f822b10b7bc0de
N <limepassion@gmail.com>
2017-09-18 13:17:32 +0300

    user edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "ads#index"
   get "users", to: "users#index"
+  get "users/:id/edit", to: "users#edit"
+  
   resources :ads
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
