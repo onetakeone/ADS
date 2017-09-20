6db67872a86c840ea7436b6c2923b199bf9fdbf6
N <limepassion@gmail.com>
2017-09-20 23:20:23 +0300

    edit bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,10 +12,10 @@
 //
 //= require rails-ujs
 //= require turbolinks
-//= require jquery3
+
 //= require jquery
-//= require jquery_ujs
-//= require popper
+
+
 //= require bootstrap-sprockets
 //= require cocoon
 //= require_tree .
