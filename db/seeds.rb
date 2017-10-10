01dfaca2f03df6654620445c87f1339bc8aab8a4
N <limepassion@gmail.com>
2017-10-10 15:08:09 +0300

    edit seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,60 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user = User.new
-user.id = 1
-user.username = 'admin'
-user.role = 'admin'
-user.email = 'admin@admin'
-user.password = 'passpass'
-user.password_confirmation = 'passpass'
-user.save!
+User.delete_all
+user = User.create(id: 1, username: 'admin', role: 'admin', email: 'admin@admin', password: 'passpass', password_confirmation: 'passpass')
+user = User.create(id: 2, username: 'user', role: 'user', email: 'user@user', password: 'passpass', password_confirmation: 'passpass')
+user = User.create(id: 3, username: 'guest', role: 'guest', email: 'guest@guest', password: 'passpass', password_confirmation: 'passpass')
+user = User.create(id: 4, username: 'roman', role: 'user', email: 'user1@user1', password: 'passpass', password_confirmation: 'passpass')
 
-user = User.new
-user.id = 2
-user.username = 'user'
-user.role = 'user'
-user.email = 'user@user'
-user.password = 'passpass'
-user.password_confirmation = 'passpass'
-user.save!
+Type.delete_all
+Type.create(id: 1, ad_type: 'WTS')
+Type.create(id: 2, ad_type: 'WTB')
+Type.create(id: 3, ad_type: 'Platforms')
 
-user = User.new
-user.id = 3
-user.username = 'guest'
-user.role = 'guest'
-user.email = 'guest@guest'
-user.password = 'passpass'
-user.password_confirmation = 'passpass'
-user.save!
-
-user = User.new
-user.id = 4
-user.username = 'roman'
-user.role = 'user'
-user.email = 'roman@roman'
-user.password = 'passpass'
-user.password_confirmation = 'passpass'
-user.save!
-
-Type.create(ad_type: 'WTS')
-Type.create(ad_type: 'WTB')
-Type.create(ad_type: 'Platforms')
-
-Ad.create(id: 1, user_id: 2, type_id: 1, state: 'draft', title: 'BTC', body: '10 BTC per 3500 USDT')
-Ad.create(id: 2, user_id: 4, type_id: 2, state: 'new', title: 'ETH', body: '100 ETH per 250 USDT')
-Ad.create(id: 3, user_id: 2, type_id: 3, state: 'verified', title: 'LTC', body: '10 LTC per 35 USDT')
-Ad.create(id: 4, user_id: 4, type_id: 1, state: 'published', title: 'BTC', body: '101 BTC per 3600 USDT')
-Ad.create(id: 5, user_id: 2, type_id: 2, state: 'archieved', title: 'DASH', body: '50 DASH per 3400 USDT')
-Ad.create(id: 6, user_id: 4, type_id: 3, state: 'denied', title: 'BTC', body: '10 BTC per 3550 USDT')
-Ad.create(id: 7, user_id: 2, type_id: 1, state: 'deleted', title: 'BTC', body: '100 BTC per 3560 USDT')
-Ad.create(id: 8, user_id: 4, type_id: 2, state: 'draft', title: 'ETH', body: '5 ETH per 270 USDT')
-Ad.create(id: 9, user_id: 2, type_id: 3, state: 'verified', title: 'LTC', body: '10 BTC per 3570 USDT')
-Ad.create(id: 10, user_id: 4, type_id: 1, state: 'published', title: 'BTC', body: '5 BTC per 3530 USDT')
-Ad.create(id: 11, user_id: 2, type_id: 2, state: 'published', title: 'BTC', body: '9 BTC per 3520 USDT')
-Ad.create(id: 12, user_id: 4, type_id: 3, state: 'published', title: 'BTC', body: '2 BTC per 3550 USDT')
-Ad.create(id: 13, user_id: 2, type_id: 1, state: 'published', title: 'BTC', body: '11 BTC per 3510 USDT')
-Ad.create(id: 14, user_id: 4, type_id: 2, state: 'published', title: 'BTC', body: '55 BTC per 3400 USDT')
+Ad.delete_all
+Ad.create(user_id: 2, type_id: 1, state: 'draft', title: 'BTC', body: '10 BTC per 3500 USDT')
+Ad.create(user_id: 4, type_id: 2, state: 'new', title: 'ETH', body: '100 ETH per 250 USDT')
+Ad.create(user_id: 2, type_id: 3, state: 'verified', title: 'LTC', body: '10 LTC per 35 USDT')
+Ad.create(user_id: 4, type_id: 1, state: 'published', title: 'BTC', body: '101 BTC per 3600 USDT')
+Ad.create(user_id: 2, type_id: 2, state: 'archieved', title: 'DASH', body: '50 DASH per 3400 USDT')
+Ad.create(user_id: 4, type_id: 3, state: 'denied', title: 'BTC', body: '10 BTC per 3550 USDT')
+Ad.create(user_id: 2, type_id: 1, state: 'deleted', title: 'BTC', body: '100 BTC per 3560 USDT')
+Ad.create(user_id: 4, type_id: 2, state: 'draft', title: 'ETH', body: '5 ETH per 270 USDT')
+Ad.create(user_id: 2, type_id: 3, state: 'verified', title: 'LTC', body: '10 BTC per 3570 USDT')
+Ad.create(user_id: 4, type_id: 1, state: 'published', title: 'BTC', body: '5 BTC per 3530 USDT')
+Ad.create(user_id: 2, type_id: 2, state: 'published', title: 'BTC', body: '9 BTC per 3520 USDT')
+Ad.create(user_id: 4, type_id: 3, state: 'published', title: 'BTC', body: '2 BTC per 3550 USDT')
+Ad.create(user_id: 2, type_id: 1, state: 'published', title: 'BTC', body: '11 BTC per 3510 USDT')
+Ad.create(user_id: 4, type_id: 2, state: 'published', title: 'BTC', body: '55 BTC per 3400 USDT')
 
 
 
