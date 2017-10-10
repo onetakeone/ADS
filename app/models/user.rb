fea77cf11703791c3bc405354ba51e31f4639aff
N <limepassion@gmail.com>
2017-10-10 15:18:12 +0300

    Used > Ad association fix (destroy)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true 
   validates :email, presence: true, uniqueness: true 
   
-  has_many :ads 
+  has_many :ads, dependent: :destroy 
 
   extend Enumerize
   enumerize :role, in: [:guest, :user, :admin], default: :guest  
