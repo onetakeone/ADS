0348c0728cce51bfb5cd94db108fc9d756fac698
N <limepassion@gmail.com>
2017-10-10 23:46:55 +0300

    Generators config

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,20 @@ Bundler.require(*Rails.groups)
 
 module ADS
   class Application < Rails::Application
+    config.generators do |g|
+      g.test_framework  false
+      g.test_unit       false
+      g.assets          false
+      g.helper          false
+      g.jbuilder        false      
+    end
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # -- all .rb files in that directory are automatically loaded.   
   end
 end
+
