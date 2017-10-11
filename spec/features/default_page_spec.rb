6a2aaae45042830f25a6827784e8eeefb6672373
N <limepassion@gmail.com>
2017-10-12 00:12:17 +0300

    Tests: sign in, create ad, edit ad

--- spec/features/default_page_spec.rb
+++ spec/features/default_page_spec.rb
@@ -1,28 +1,48 @@
-# require "rails_helper"
-# include Warden::Test::Helpers
+require 'rails_helper'
+include Warden::Test::Helpers
 
-# feature "default" do
-#   scenario "default" do
-#     visit '/ads/'
-#       expect(page).to have_xpath("/html/body/hh")
-#     sign_in
-#     visit '/ads/new'
-#       type_in
-#       fill_in 'Title', with: 'Title-test'
-#       fill_in 'Advert', with: 'Advert-test'
-#       click_button 'done'
-#       expect(page).to have_content 'created'
-#   end
-# end
+feature 'Default ' do
+  scenario 'Open main page' do
+    visit '/ads/'
+    expect(page).to have_xpath("/html/body/hh")
+  end
 
-# #sign in via Warden helper login_as
-# def sign_in
-#   visit new_user_session_path
-#   user = FactoryGirl.create(:user)
-#   login_as(user)
-# end
+  scenario 'Sign in' do
+    sign_in
+  end
 
-# def type_in
-#   type = FactoryGirl.create(:type)
-#   select type.ad_type, :from => 'Type'
-# end
+  scenario 'Create ad' do
+    sign_in
+    create_ad
+  end
+
+  scenario 'Edit ad' do
+    sign_in
+    create_ad
+    edit_ad
+  end
+end
+
+def sign_in
+  visit new_user_session_path
+  user = FactoryGirl.create(:user)
+  login_as(user) # Sign in via Warden helper login_as
+end
+
+def create_ad
+  type = FactoryGirl.create(:type)
+  visit '/ads/new'
+  select type.ad_type, from: 'Type'
+  fill_in 'Title', with: 'Title-test'
+  fill_in 'Advert', with: 'Advert-test'
+  click_button 'done'
+  expect(page).to have_content 'Ad was successfully created'
+end
+
+def edit_ad
+  click_on 'edit'
+  expect(page).to have_content 'Editing Ad'
+  select 'new', from: 'State'
+  click_button 'done'
+  expect(page).to have_content 'Ad was successfully updated'
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,8 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include StateMachinesRspec::Matchers
 
+  #config.raise_errors_for_deprecations!
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
