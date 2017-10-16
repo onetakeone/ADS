7251d8e1cfbd52ec516dd65af5b336595dc3906c
N <limepassion@gmail.com>
2017-10-16 23:52:11 +0300

    cancan abilithies to methods

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,52 +2,38 @@ class Ability
   include CanCan::Ability
 
   def initialize user
-    user ||= User.new
-    if user.role.admin?
+    @user ||= User.new
+    if @user.role.admin?
+      admin_roles
+    elsif @user.role.user?
+      user_roles
+    else
+      guest_roles
+    end
+  end
+
+  private
+    def admin_roles
       can :manage, User
       can :create, User
       can :manage, Type
       can :read, Ad
-      can :destroy, Ad      
-      can :update, Ad  
-    elsif user.role.user?
-      can :create, Ad, user_id: user.id
+      can :destroy, Ad
+      can :update, Ad
+    end
+
+    def user_roles
+      can :create, Ad, user_id: @user.id
       can :read, Ad
-      can :open, Ad 
-      can :update, Ad, user_id: user.id, :state => ['draft', 'archieved'] 
-      can :destroy, Ad, user_id: user.id, :state => ['draft', 'new', 'verified', 'published']
+      can :open, Ad
+      can :update, Ad, user_id: @user.id, :state => ['draft', 'archieved']
+      can :destroy, Ad, user_id: @user.id, :state => ['draft', 'new', 'verified', 'published']
       cannot :edit, Type
-    else
+    end
+
+    def guest_roles
       can :read, Ad
       cannot :edit, Ad
     end
-  end
 end
-
-# Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    # can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/ryanb/cancan/wiki/Defining-Abilities
-    
+ 
